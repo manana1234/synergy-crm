4d8ade39b222f56caac767f071c16b0118b76d4a
Logan <96558780+manana1234@users.noreply.github.com>
2023-03-28 18:24:55 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmLngRqsQ5XQx9MXisLjue7g2BdXVg-ww",
-    authDomain: "synergy-crm-4eacd.firebaseapp.com",
-    projectId: "synergy-crm-4eacd",
-    storageBucket: "synergy-crm-4eacd.appspot.com",
-    messagingSenderId: "205542752701",
-    appId: "1:205542752701:web:3f7155e6f12fb7085e52d4",
-    measurementId: "G-FW6RBWD97V"
+    apiKey: " ",
+    authDomain: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
 };
 
 firebase.initializeApp(firebaseConfig);
